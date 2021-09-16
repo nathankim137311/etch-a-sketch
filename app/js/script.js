0ee02cefebb8792469421db52fae8c4c0ec2af33
web-dev <nathankim137311@gmail.com>
2021-09-15 22:06:03 -0700

    added random color function and added reset button

--- app/js/script.js
+++ app/js/script.js
@@ -1,47 +1,67 @@
 console.log('hello everybody');
 
 // variables 
+const resetBtn = document.getElementById('reset'); 
 const containerDiv = document.getElementById('container');
-const gridArr = [];
-let grid = createGrid(16, 16); 
+const grid = document.getElementsByClassName('grid'); 
 
 // event listeners 
+resetBtn.addEventListener('click', () => {
+  let gridDimensions = prompt('Select Width/Height (squares)', '0'); 
+  let squares = parseInt(gridDimensions); 
+  if (gridDimensions === '' || gridDimensions === null || gridDimensions == 0) {
+    alert('ERROR'); 
+  } else {
+    clearGrid(); 
+    createGrid(squares); 
+  }
+});
 
 // functions 
-function createGrid (rows, columns) {
+function createGrid (squares) {
   let i = 1;
-  while (i < rows * columns) {
-    let gridCell = containerDiv.appendChild(document.createElement('div'));  
-    gridArr.push(gridCell); 
+  while (i <= Math.pow(squares, 2)) {
+    let square = containerDiv.appendChild(document.createElement('div'));
+    square.className += 'grid';  
     i++; 
   }
-  return
+  for (let i = 0; i < grid.length; i++) {
+    grid[i].addEventListener('mouseenter', () => {
+      grid[i].classList.add('tile'); 
+      grid[i].style.backgroundColor = randomColor(); 
+    }); 
+  }
 }
 
-for (let i = 0; i < gridArr.length; i++) {
-  gridArr[i].addEventListener('mouseenter', () => {
-    gridArr[i].classList.add('tile'); 
-    gridArr[i].style.backgroundColor = 'black'; 
-  }); 
+//createGrid(16); 
+//console.log(grid.length); 
+
+const clearGrid = (array) => array.forEach(div => div.remove()); 
+
+clearGrid(document.getElementsByClassName('grid')); 
+
+function random(min,max) {
+  const num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
 }
 
-function removeTransition (e) {
-  if (e.propertyName !== 'transform') return; 
-  this.classList.remove('tile'); 
-  this.style.backgroundColor = 'white'; 
+function randomColor() {
+  return 'rgb(' + random(0,255) + ', ' + random(0,255) + ', ' + random(0,255) +  ')';
 }
-
-gridArr.forEach(cell => cell.addEventListener('transitionend', removeTransition)); 
-
-
-
-
-//function tilesBlack () {
-//  for (let i = 0; i < gridArr.length; i++) {
-//    gridArr[i].addEventListener('mouseender', (e) => {
-//      e.target.style.color = 'black';
-//    }); 
-//  }
+ 
+//for (let i = 0; i < gridArr.length; i++) {
+//  gridArr[i].addEventListener('mouseenter', () => {
+//    gridArr[i].classList.add('tile'); 
+//    gridArr[i].style.backgroundColor = 'black'; 
+//  }); 
 //}
+
+//function removeTransition (e) {
+//  if (e.propertyName !== 'transform') return; 
+//  this.classList.remove('tile'); 
+//  this.style.backgroundColor = 'white'; 
+//}
+
+
 
 
